926bbeb57da26e56119f790fa8dd7e60cb10ae44
no-ear <no_ear@server.fake>
2014-01-11 17:22:47 +0900

    Under construction, part version detail view. Add file upload.

--- app/controllers/PartVersionDetailController.java
+++ app/controllers/PartVersionDetailController.java
@@ -1,6 +1,12 @@
 package controllers;
 
+import java.io.File;
+import java.util.HashMap;
+
+import models.Document;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.PartVersionDetail;
@@ -22,7 +28,28 @@ public class PartVersionDetailController extends Controller {
 	}
 
 	public static Result upload(final long id) {
-		return TODO;
+		MultipartFormData body = request().body().asMultipartFormData();
+
+		FilePart picture = body.getFile("upfile");
+
+		if (picture != null) {
+			// String fileName = picture.getFilename();
+			// String contentType = picture.getContentType();
+			File file = picture.getFile();
+
+			HashMap<String, Object> properties = new HashMap<String, Object>();
+			try {
+				Document.buildDocument(properties, file);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return badRequest();
+			}
+
+			return ok("File uploaded");
+		} else {
+			flash("error", "Missing file");
+			return redirect(routes.Application.index());
+		}
 	}
 
 }
--- app/models/Document.java
+++ app/models/Document.java
@@ -1,5 +1,11 @@
 package models;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -57,4 +63,56 @@ public final class Document extends Model {
 	 * Serial Version ID.
 	 */
 	private static final long serialVersionUID = -1695193238431975776L;
+
+	public static Document buildDocument(
+			final HashMap<String, Object> properties, final File file) throws Exception {
+		Document document = new Document();
+
+		Field[] fields = Document.class.getFields();
+
+		for (Field field : fields) {
+			String key = "Document." + field.getName();
+			if (!properties.containsKey(key)) {
+				continue;
+			}
+
+			Object object = properties.get(key);
+			try {
+				field.set(document, object);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				// No reachable code?
+				e.printStackTrace();
+			}
+		}
+
+		document.file = readFileToByte(file.getPath());
+
+		document.save();
+
+		return document;
+	}
+
+	/**
+	 * File to byte[].
+	 * 
+	 * @param filePath
+	 *            Target file path
+	 * @return byte array
+	 * @throws Exception
+	 *             Not found file, Access error, etc.
+	 */
+	private static byte[] readFileToByte(final String filePath)
+			throws Exception {
+		byte[] b = new byte[1];
+		FileInputStream fis = new FileInputStream(filePath);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		while (fis.read(b) > 0) {
+			baos.write(b);
+		}
+		baos.close();
+		fis.close();
+		b = baos.toByteArray();
+
+		return b;
+	}
 }
